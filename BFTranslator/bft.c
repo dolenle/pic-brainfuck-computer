0908103895b762f897104279d49acd2f5b8c896a
Dolen Le <le2@cooper.edu>
2015-09-04 15:05:49 -0400

    Added BF-to-hex converter

---
+++ BFTranslator/bft.c
@@ -0,0 +1,175 @@
+//Brainfuck-to-BIN Translator for PIC Brainfuck Computer
+
+//Instruction Decode as follows:
+//000 +
+//001 ,
+//010 -
+//011 .
+//100 <
+//101 ]
+//110 >
+//111 [
+//0x0F EOF
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct binData {
+	char data;
+	struct binData *next;
+};
+
+void printUsage(char *s);
+struct binData *newBin(struct binData *prev);
+FILE *in, *out;
+int format = 0; //text or binary output
+
+int main(int argc, char *argv[]) {
+	in = stdin;
+	out = stdout;
+	if(argc > 1 && argc <= 6) {
+		int i;
+		for(i = 1; i < argc; i++) {
+			if(!strcmp(argv[i], "-i")) {
+				if(i+1 < argc) {
+					in = fopen(argv[i+1], "r");
+					if(!in) {
+						fprintf(stderr, "Could not open %s\n", argv[i+1]);
+						return 1;
+					}
+					i++;
+				} else {
+					printUsage(argv[0]);
+					return 1;
+				}
+			} else if(!strcmp(argv[i], "-o")) {
+				if(i+1 < argc) {
+					out = fopen(argv[i+1], "wb");
+					if(!out) {
+						fprintf(stderr, "Could not open %s\n", argv[i+1]);
+						return 1;
+					}
+					i++;
+				} else {
+					printUsage(argv[0]);
+					return 1;
+				}
+			} else if(!strcmp(argv[i], "-b")) {
+				format = 1;
+			} else {
+				printUsage(argv[0]);
+				return 1;
+			}
+		}
+	} else if(argc != 1) {
+		printUsage(argv[0]);
+		return 1;
+	}
+	
+	//Parse the input and write the output
+	char inst, temp;
+	struct binData *start = NULL;
+	struct binData *current = NULL;
+	int instCount = 0;
+	int byteCount = 0;
+	int flag = 0;
+	
+	while(fscanf(in, "%c", &inst) != EOF) {
+		switch(inst) {
+			case '+':
+				temp = 0x00;
+				break;
+			case '-':
+				temp = 0x02;
+				break;
+			case '<':
+				temp = 0x04;
+				break;
+			case '>':
+				temp = 0x06;
+				break;
+			case '[':
+				temp = 0x07;
+				break;
+			case ']':
+				temp = 0x05;
+				break;
+			case '.':
+				temp = 0x03;
+				break;
+			case ',':
+				temp = 0x01;
+				break;
+			default:
+				flag = 1;
+		}
+		if(!flag) {
+			if(instCount%2 == 0) {
+				current = newBin(current);
+				if(!start)
+					start = current;
+				byteCount++;
+				current->data = temp << 4;
+			} else {
+				current->data = current->data | temp;
+			}
+			instCount++;
+		} else {
+			flag = 0; //ignore other chars
+		}
+	}
+	//append EOF instruction
+	if(instCount%2 == 0) {
+		current = newBin(current);
+		if(!start)
+			start = current;
+		byteCount++;
+		current->data = 0xF0;
+	} else {
+		current->data = current->data | 0x0F;
+	}
+	
+	
+	if(format == 0) {
+		while(start) {
+			fprintf(out, "0x%hhX", start->data);
+			if(start->next) {
+				fprintf(out, ",");
+				start = start->next;
+			} else {
+				break;
+			}
+		}
+		fprintf(out, "\n");
+	} else if(format == 1) {
+		while(start) {
+			fwrite(&(start->data), 1, sizeof(char), out);
+			if(start->next) {
+				start = start->next;
+			} else {
+				break;
+			}
+		}
+	}
+	fprintf(stderr, "%d bytes used.\n", byteCount);
+}
+
+struct binData *newBin(struct binData *prev) {
+	struct binData *bin;
+	if(!(bin=malloc(sizeof(struct binData)))) {
+		fprintf(stderr, "Error: Cannot allocate memory.\n");
+		return NULL;
+	}
+	bin->next = NULL;
+	if(prev)
+		prev->next = bin;
+	return bin;
+}
+
+void printUsage(char *s) {
+	fprintf(stderr, "Usage: %s [-i input_file] [-o output_file] [-b]\n", s);
+}
+
+char swap(char c) {
+	return ((c & 0xF0 >> 4) | (c & 0x0F << 4));
+}
--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -86,7 +86,7 @@ ${OBJECTDIR}/main.o: main.asm  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/main.o.d 
 	@${RM} ${OBJECTDIR}/main.o 
-	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/main.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_PK3=1 -q -p$(MP_PROCESSOR_OPTION) -u  -l\\\"${OBJECTDIR}/main.lst\\\" -e\\\"${OBJECTDIR}/main.err\\\" $(ASM_OPTIONS)   -o\\\"${OBJECTDIR}/main.o\\\" \\\"main.asm\\\" 
+	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/main.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_SIMULATOR=1 -q -p$(MP_PROCESSOR_OPTION) -u  -l\\\"${OBJECTDIR}/main.lst\\\" -e\\\"${OBJECTDIR}/main.err\\\" $(ASM_OPTIONS)   -o\\\"${OBJECTDIR}/main.o\\\" \\\"main.asm\\\" 
 	@${DEP_GEN} -d "${OBJECTDIR}/main.o"
 	@${FIXDEPS} "${OBJECTDIR}/main.o.d" $(SILENT) -rsi ${MP_AS_DIR} -c18 
 	
@@ -106,7 +106,7 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/BFComputer.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION)  -w -x -u_DEBUG -z__ICD2RAM=1 -m"${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map"   -z__MPLAB_BUILD=1  -z__MPLAB_DEBUG=1 -z__MPLAB_DEBUGGER_PK3=1 $(MP_LINKER_DEBUG_OPTION) -odist/${CND_CONF}/${IMAGE_TYPE}/BFComputer.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
+	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION)  -w -x -u_DEBUG -z__ICD2RAM=1 -m"${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map"   -z__MPLAB_BUILD=1  -z__MPLAB_DEBUG=1 -z__MPLAB_DEBUGGER_SIMULATOR=1 $(MP_LINKER_DEBUG_OPTION) -odist/${CND_CONF}/${IMAGE_TYPE}/BFComputer.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/BFComputer.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
